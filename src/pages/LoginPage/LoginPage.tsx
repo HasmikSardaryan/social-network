012f27ac3ee9add57db7f90f3c66e607022e80c0
Hasmik <hasmiksardaryan20@gmail.com>
2025-09-30 21:57:17 -0700

    sync

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -30,6 +30,7 @@ export const Login = () => {
       });
   };
 
+  
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-slate-900 to-slate-800 p-6">
       <motion.div
